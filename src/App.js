c10dc19117e7b5ef1c3c21003b0549909edbde89
D1S3CT <Deifordhaze@gmail.com>
2025-04-17 09:01:44 +0400

    Pre-Settings Pushed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { v4 as uuidv4 } from 'uuid'
+import { useState } from 'react'
+import './App.css'
+import Todo from './components/Todos/Todo'
+import TodoForm from './components/Todos/TodoForm'
+import TodoList from './components/Todos/TodoList'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const addTodoHandler = (text) => {
+    const newTodo = {
+      text,
+      isCompleted: false,
+      id: uuidv4(),
+    }
+    setTodos([...todos, text])
+  }
+  const deleteTodoHandler = (index) => {
+    setTodos(todos.filter((_, idx) => idx !== index))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App</h1>
+      <TodoForm addTodo={addTodoHandler} />
+      <TodoList todos={todos} deleteTodo={deleteTodoHandler} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
